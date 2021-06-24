23e37dcf217549f9363df37a01021cb746c3147e
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-06-25 00:41:51 +0100

    routes estabelished

--- server.js
+++ server.js
@@ -11,3 +11,23 @@ const methodOverride = require('method-override');
 const indexRouter = require('./routes/index');
 const authorRouther = require('./routes/authors');
 const bookRouter = require('./routes/books');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layout/layout');
+app.use(expressLayout);
+app.use(methodOverride('__method'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+app.use('/', indexRouter);
+app.use('/authors', authorRouther);
+app.use('/books', bookRouter);
+
+app.listen(process.env.PORT || 3000);
